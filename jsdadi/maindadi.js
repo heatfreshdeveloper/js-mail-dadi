aa165c742c6f10c13bcedb2f996a1c52c601a746
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-13 18:02:25 +0200

    Completato lo script corretto del gioco dadi

--- jsdadi/maindadi.js
+++ jsdadi/maindadi.js
@@ -4,13 +4,15 @@ const userElement = document.querySelector(".my-user");
 const computerElement = document.querySelector(".the-computer");
 
 btnSubmit.addEventListener("click", function () { 
-    let user = document.getElementById("my-demo").innerHTML =
-    Math.floor(Math.random() * 10) + 1;
+    let userScore = Math.floor(Math.random() * 6) + 1;
+    let user = document.getElementById("my-demo");
+    user.innerHTML = userScore
 
-    let computer = document.getElementById("my-demo2").innerHTML =
-    Math.floor(Math.random() * 10) + 1;
+    let computerScore = Math.floor(Math.random() * 6) + 1;
+    let computer = document.getElementById("my-demo2");
+    computer.innerHTML = computerScore
 
-        if(user > computer) {
+        if(userScore > computerScore) {
             alert("Ha vinto il Giocatore")
         } else {
             alert("Il giocatore ha perso!")
