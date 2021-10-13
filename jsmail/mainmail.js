4f4879b1810d13e70756dac944d244522173352a
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-13 16:24:40 +0200

    Completato il file js del programma mail

---
+++ jsmail/mainmail.js
@@ -0,0 +1,18 @@
+const mailUser = prompt("Qual è il tuo indirizzo email?");
+
+const mailList = ["gigiostrada@gmail.com", "luigiverdi@libero.it", "mariorossi@hotmail.com", "alexbernardi@gmail.com"]
+
+for (let i = 0; i < mailList.length; i++) {
+
+    mailList[i];
+  
+    const mailGuide = mailList[i];
+  
+    console.log(mailList[i])
+
+    if(mailGuide.includes(mailUser)) {
+        alert("Benvenuto nel sito!")
+    }
+
+  }
+  
